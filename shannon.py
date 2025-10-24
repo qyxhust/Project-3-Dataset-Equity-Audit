758eabf06465dd8cc08d121a734c2e372c7d8198
Cyan Ding <blueplus.d@gmail.com>
2025-10-23 18:06:30 -0700

    fix deduplication bug

--- shannon.py
+++ shannon.py
@@ -5,7 +5,10 @@ import math
 df = pd.read_csv("population_summary.csv")
 
 shannon_sub = -sum(row * math.log(row) for row in df["percent"])
-shannon_super = -sum(row * math.log(row) for row in df["super_percent"])
+
+deduplicated = df.drop_duplicates(subset=["super_pop"], keep="first")
+
+shannon_super = -sum(row * math.log(row) for row in deduplicated["super_percent"])
 
 print(f" Shannon sub: {shannon_sub}, Shannon super: {shannon_super}")
 
@@ -13,8 +16,7 @@ print(f" Shannon sub: {shannon_sub}, Shannon super: {shannon_super}")
 """
 Notes: 
 
-Shannon sub: 3.2765669523856547, Shannon super: 8.363839112542903
-
+Shannon sub: 3.2765669523856547, Shannon super: 1.5652133147716714
 Both Shannon indicies for the sub and super populations are pretty high, indicating that the dataset is pretty diverse geographically-wise
 
 """
